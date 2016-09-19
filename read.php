535a4b62c2522d2ef02f1914a48331beeff12802
guava25 <guave753@outlook.com>
2016-09-18 22:32:28 -0500

    Read Table
    Get images to table

--- read.php
+++ read.php
@@ -5,17 +5,25 @@ $collection = $db->photos;
 
 $photo_name = $_GET['photo'];
 
-$cursor = $collection->find(array("name" => $photo_name));
+$cursor = $collection->find();
 
 if(empty($cursor)){
     echo "Empty";
 }
 
+?>
+<table class="table">
+    
+<?php
 foreach ($cursor as $document) {
+    echo "<tr><td>";
     $photoData = $document['photo'];
     $decode = base64_decode($photoData);
     header('Content-type: image/jpeg');
     echo $decode;
+    echo "</td></tr>";
 }
 
 ?>
+    
+</table>
