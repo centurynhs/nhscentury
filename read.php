3e31777e0e22fc94c14fe557529b2dbc275f19a4
guava25 <guave753@outlook.com>
2016-09-18 21:07:38 -0500

    New Method
    New Reading Method

--- read.php
+++ read.php
@@ -3,14 +3,15 @@ $connect = new MongoClient("mongodb://admin:nhsadmin@ds147995.mlab.com:47995/cen
 $db = $connect->centurynhs;
 $collection = $db->photos;
 
-$cursor = $collection->find();
+$photo_name = $_GET['photo'];
+
+$cursor = $collection->find(array("name" => $photoname));
 
 foreach ($cursor as $document) {
     $photoData = $document['photo'];
     $decode = base64_decode($photoData);
     header('Content-type: image/jpeg');
     echo $decode;
-    echo "<br>";
 }
 
 ?>
